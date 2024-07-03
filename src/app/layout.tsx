b6bb1153149ef6208c6e874e8bdde54ac288fbf7
dhimmar_ankit <dhimmarankit786@gmail.com>
2024-07-03 17:57:08 +0530

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Portfolio",
+    description: "Portfolio using React.Js, Tailwind and Typescript",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} bg-gray-50 text-gray-950 relative`}
+            >
+                <div className="bg-[#FBE2E3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+                <div className="bg-[#DBD7FB] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+                {children}
+            </body>
+        </html>
+    );
+}
