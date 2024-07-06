31383b426fb6faf4efe77d1dea98b10e3ea3fcea
dhimmar_ankit <dhimmarankit786@gmail.com>
2024-07-06 19:35:19 +0530

    Website is done.

--- components/project.tsx
+++ components/project.tsx
@@ -28,8 +28,8 @@ export default function Project({
                 ref={ref}
                 style={{ scale: scaleProgress, opacity: opacityProgress }}
             >
-                <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]  group-even:pl-8 hover:bg-gray-200 transition sm:group-even:pl-8">
-                    <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+                <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8">
+                    <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
                         <h3 className="text-2xl font-semibold">{title}</h3>
                         <p className="mt-2 leading-relaxed text-gray-700">
                             {description}
@@ -46,7 +46,8 @@ export default function Project({
                         </ul>
                     </div>
                     <Image
-                        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 transition group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+                        className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2
+                        group-even:right-[initial] group-even:-left-40"
                         src={imageUrl}
                         alt={title}
                     />
